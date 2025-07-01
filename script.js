8d976a1be7c8257cda2ae4e6be845452872e3877
Kecio Caetano Barbosa <kecio.kecio@gmail.com>
2025-07-01 06:27:18 -0400

    inicial logic developed

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+const main = document.querySelector('main');
+
 const container = document.createElement('div');
 container.classList.add('container');
+
+let limit = 16;
+
+for (let i = 0; i < limit; i++) {
+    const div = document.createElement('div');
+    div.textContent = `${i}`;
+    div.classList.add('div');
+    container.appendChild(div);
+}
+
+main.appendChild(container);
